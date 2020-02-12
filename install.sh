7dec9b3206276ba7cd63dd80e82049d48902b9f1
LilRedNurgling <johnellis173@gmail.com>
2020-02-12 13:37:00 -0500

    Install script should actually work all on its own now, provided everything in the directory is accessible to the server

--- install.sh
+++ install.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+# Get the necessary packages, namely Oracle Apache2, MySQL Server, and PHP
 apt-get update
 apt-get install apache2 mysql-server libapache2-mod-php5 php5-mysql
 
+# Open to both HTTP and HTTPS traffic. Both must be enabled, even if HTTP just redirects to HTTPS
+ufw enable
 ufw allow http
 ufw allow https
 ufw reload
 
-mysql -u root -p -e "CREATE DATABASE cnt440901secweb; CREATE USER web-user@localhost; GRANT SELECT ON cnt440901secweb TO web-user@localhost;"
+# Load the given database into MySQL for use by the web server
+mysql -u root -p -e "CREATE DATABASE cnt440901secweb; CREATE USER 'web-user'@'localhost'; GRANT SELECT ON cnt440901secweb.* TO 'web-user'@'localhost';"
 mysql -u root -p cnt440901secweb < cnt440901secweb.sql
+
+# Copy all the necessary files given here, overriding anything already there
+cp -rf etc/* /etc
+cp -rf var/* /var
+
+# Create a symbolic link to the HTTPS site in sites-enabled for it to become available
+ln -s /etc/apache2/sites-available/000-default-ssl.conf /etc/apache2/sites-enabled/000-default-ssl.conf
+
+# Get rid of the default web page separately since index.php is now used over index.html according to .htaccess
+rm /var/www/html/index.html
+
+# Enable the site to use SSL and the redirect functionalities inside .htaccess
+a2enmod ssl
+a2enmod rewrite
+
+# Restart the web server to apply the changes
+service apache2 restart
