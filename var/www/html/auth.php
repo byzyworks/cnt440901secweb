c17f5db7c4a2134873e2cf8a1e2e69d7a3b2afaa
LilRedNurgling <johnellis173@gmail.com>
2020-03-08 20:45:05 -0400

    Removed html content from auth.php and reg.php (these pages should only be used as redirects, and even the error messages will later be put on different pages)

--- var/www/html/auth.php
+++ var/www/html/auth.php
@@ -1,60 +1,48 @@
 <?php
 	session_start();
+
+	$sql_server = "localhost";
+	$sql_uname  = "web-user";
+	$sql_passwd = "";
+	$sql_db     = "cnt440901secweb";
+	$usr_table  = "users";
+	$usr_uname  = $_POST['uname'];
+	$usr_passwd = $_POST['passwd'];
+	$ip         = $_SERVER['SERVER_ADDR'];
+	
+	// Create a session variable to be used across web pages
+	$_SESSION['uname'] = $usr_uname;
+	
+	// Create connection
+	$conn = new mysqli($sql_server, $sql_uname, $sql_passwd, $sql_db);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+		//header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+	}
+
+	// Query database for user (insecure)
+	//$sql = "SELECT id, uname, hash, bio FROM $usr_table WHERE uname='$usr_uname'";
+	//$result = $conn->query($sql);
+	
+	// Query database for user
+	$stmt = $conn->prepare("SELECT id, uname, hash, bio FROM $usr_table WHERE uname = ?");
+	$stmt->bind_param("s", $usr_uname);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	
+	// Check if user exists
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			// Verify their password
+			if (password_verify($usr_passwd, $row["hash"])) {
+				$conn->close();
+				header("Location: https://$ip/account.php");
+			}
+		}
+	}
+
+	$conn->close();
+	echo "Username or password is incorrect."
 ?>
-
-<!doctype html>
-
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Login</title>
-	</head>
-	<body>
-		<?php
-			$sql_server = "localhost";
-			$sql_uname  = "web-user";
-			$sql_passwd = "";
-			$sql_db     = "cnt440901secweb";
-            $usr_table  = "users";
-			$usr_uname  = $_POST['uname'];
-			$usr_passwd = $_POST['passwd'];
-			$ip         = $_SERVER['SERVER_ADDR'];
-			
-            // Create a session variable to be used across web pages
-			$_SESSION['uname'] = $usr_uname;
-			
-			// Create connection
-			$conn = new mysqli($sql_server, $sql_uname, $sql_passwd, $sql_db);
-
-			// Check connection
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-                //header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-			}
-
-			// Query database for user (insecure)
-			//$sql = "SELECT id, uname, hash, bio FROM $usr_table WHERE uname='$usr_uname'";
-			//$result = $conn->query($sql);
-			
-            // Query database for user
-			$stmt = $conn->prepare("SELECT id, uname, hash, bio FROM $usr_table WHERE uname = ?");
-			$stmt->bind_param("s", $usr_uname);
-			$stmt->execute();
-			$result = $stmt->get_result();
-			
-            // Check if user exists
-			if ($result->num_rows > 0) {
-				while ($row = $result->fetch_assoc()) {
-                    // Verify their password
-				    if (password_verify($usr_passwd, $row["hash"])) {
-                        $conn->close();
-                        header("Location: https://$ip/account.php");
-                    }
-				}
-			}
-
-            $conn->close();
-            echo "Username or password is incorrect."
-		?>
-	</body>
-</html>
--- var/www/html/reg.php
+++ var/www/html/reg.php
@@ -1,60 +1,48 @@
 <?php
 	session_start();
+
+	$sql_server = "localhost";
+	$sql_uname  = "web-user";
+	$sql_passwd = "";
+	$sql_db     = "cnt440901secweb";
+	$usr_table  = "users";
+	$usr_uname  = $_POST['uname'];
+	$usr_passwd = $_POST['passwd'];
+	$ip         = $_SERVER['SERVER_ADDR'];
+	
+	// Create connection
+	$conn = new mysqli($sql_server, $sql_uname, $sql_passwd, $sql_db);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+		//header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+	}
+	
+	//
+	$cost       = 12;
+	$usr_passwd = password_hash($usr_passwd, PASSWORD_BCRYPT, ["cost" => $cost]);
+	
+	// 
+	$stmt = $conn->prepare("INSERT INTO $usr_table (uname, hash) VALUES (?, ?)");
+	$stmt->bind_param("ss", $usr_uname, $usr_passwd);
+	$stmt->execute();
+	
+	// Check if user exists
+	/*
+	if ($result->num_rows > 0) {
+		$ip = $_SERVER['SERVER_ADDR'];
+		while ($row = $result->fetch_assoc()) {
+			// Verify their password
+			if (password_verify($usr_passwd, $row["hash"])) {
+				$conn->close();
+				header("Location: https://$ip/account.php");
+			}
+		}
+	}
+	*/
+
+	$conn->close();
+	header("Location: https://$ip/index.php");
+	//echo "Username or password is incorrect."
 ?>
-
-<!doctype html>
-
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Register</title>
-	</head>
-	<body>
-		<?php
-			$sql_server = "localhost";
-			$sql_uname  = "web-user";
-			$sql_passwd = "";
-			$sql_db     = "cnt440901secweb";
-            $usr_table  = "users";
-			$usr_uname  = $_POST['uname'];
-			$usr_passwd = $_POST['passwd'];
-			$ip         = $_SERVER['SERVER_ADDR'];
-			
-			// Create connection
-			$conn = new mysqli($sql_server, $sql_uname, $sql_passwd, $sql_db);
-
-			// Check connection
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-                //header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-			}
-			
-			//
-			$cost       = 12;
-			$usr_passwd = password_hash($usr_passwd, PASSWORD_BCRYPT, ["cost" => $cost]);
-			
-            // 
-			$stmt = $conn->prepare("INSERT INTO $usr_table (uname, hash) VALUES (?, ?)");
-			$stmt->bind_param("ss", $usr_uname, $usr_passwd);
-			$stmt->execute();
-			
-            // Check if user exists
-			/*
-			if ($result->num_rows > 0) {
-				$ip = $_SERVER['SERVER_ADDR'];
-				while ($row = $result->fetch_assoc()) {
-                    // Verify their password
-				    if (password_verify($usr_passwd, $row["hash"])) {
-                        $conn->close();
-                        header("Location: https://$ip/account.php");
-                    }
-				}
-			}
-			*/
-
-            $conn->close();
-			header("Location: https://$ip/index.php");
-            //echo "Username or password is incorrect."
-		?>
-	</body>
-</html>
