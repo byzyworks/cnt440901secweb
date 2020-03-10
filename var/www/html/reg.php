eaef4251096a5d5c4e4512fa6fb84c1f35a451b3
LilRedNurgling <johnellis173@gmail.com>
2020-03-10 17:33:25 -0400

    Cleaned up reg.php

--- var/www/html/reg.php
+++ var/www/html/reg.php
@@ -1,14 +1,14 @@
 <?php
 	session_start();
 
-	$sql_server = "localhost";
-	$sql_uname  = "web-user";
-	$sql_passwd = "";
-	$sql_db     = "cnt440901secweb";
-	$usr_table  = "users";
+	$ip         = $_SERVER['SERVER_ADDR'];
+	$sql_server = 'localhost';
+	$sql_uname  = 'web-user';
+	$sql_passwd = '';
+	$sql_db     = 'cnt440901secweb';
+	$usr_table  = 'users';
 	$usr_uname  = $_POST['uname'];
 	$usr_passwd = $_POST['passwd'];
-	$ip         = $_SERVER['SERVER_ADDR'];
 	
 	// Create connection
 	$conn = new mysqli($sql_server, $sql_uname, $sql_passwd, $sql_db);
@@ -19,30 +19,17 @@
 		//header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 	}
 	
-	//
+	// Hash the new user's password
 	$cost       = 12;
 	$usr_passwd = password_hash($usr_passwd, PASSWORD_BCRYPT, ["cost" => $cost]);
 	
-	// 
+	// Attempt to add the new user's credentials to the database
 	$stmt = $conn->prepare("INSERT INTO $usr_table (uname, hash) VALUES (?, ?)");
 	$stmt->bind_param("ss", $usr_uname, $usr_passwd);
 	$stmt->execute();
-	
-	// Check if user exists
-	/*
-	if ($result->num_rows > 0) {
-		$ip = $_SERVER['SERVER_ADDR'];
-		while ($row = $result->fetch_assoc()) {
-			// Verify their password
-			if (password_verify($usr_passwd, $row["hash"])) {
-				$conn->close();
-				header("Location: https://$ip/account.php");
-			}
-		}
-	}
-	*/
 
+	// Close the connection and go back
 	$conn->close();
 	header("Location: https://$ip/index.php");
-	//echo "Username or password is incorrect."
+	exit;
 ?>
