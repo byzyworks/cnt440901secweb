30639ede38ba9c0d608d94c4ce47bd5c2b4000b1
LilRedNurgling <johnellis173@gmail.com>
2020-04-03 14:40:15 -0400

    Moved all the style information over to an external stylesheet, and changed the theme of the secure version to be blue

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -17,49 +17,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Network Security (CNT4409.01 I&T) Secure Website Project</title>
-		<style>
-			*
-			{
-				font-family: courier;
-				font-size: 125%;
-			}
-			input[type=text], input[type=password]
-			{
-				width: 100%;
-				padding: 12px 20px;
-				margin: 8px 0;
-				display: inline-block;
-				border: 1px solid #ccc;
-				box-sizing: border-box;
-			}
-			button
-			{
-				background-color: #4CAF50;
-				color: white;
-				padding: 14px 20px;
-				margin: 8px 0;
-				border: none;
-				cursor: pointer;
-				width: 100%;
-			}
-			button:hover
-			{
-				opacity: 0.8;
-			}
-			.borderless_container
-			{
-				padding: 16px;
-				font-size: 100%;
-			}
-			.container
-			{
-				border: 3px solid #f1f1f1;
-				width: 800px;
-				padding: 16px;
-			}
-		</style>
-		<script>
-		</script>
+		<link rel="stylesheet" type="text/css" href="/style.css" />
 	</head>
 	<body>
 		<div class="container">
@@ -89,7 +47,5 @@
 		<div class="container">
 			<img src="/images/welcome.jpg" style="max-width:100%;"/>
 		</div>
-		<script>
-		</script>
 	</body>
 </html>
